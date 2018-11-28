e68ece1517762ad3fa39523d7b034aeacf6dc621
jeansom <jsomalwar@gmail.com>
2018-11-27 21:30:12 -0500

    Add files via upload

--- fitFunctions.py
+++ fitFunctions.py
@@ -8,6 +8,11 @@ def fitSine2Slope( para, t, t0=-999 ):
     a11, a12, f1, a21, a22, f2 = para
     return a11*np.sin(2*np.pi*f1*(t-t0)) + a12*np.cos(2*np.pi*f1*(t-t0)) + a21*np.sin(2*np.pi*f2*(t-t0)) + a22*np.cos(2*np.pi*f2*(t-t0))
 
+def fitSine2Slope2( para, t, t0=-999 ):
+    if t0 == -999: t0 = 0
+    a1, f1, phi1, a2, f2, phi2 = para
+    return a1*np.sin(2*np.pi*f1*(t-t0) + phi1) + a2*np.sin(2*np.pi*f2*(t-t0) + phi2)
+
 def least_sq( y, yfit ):
     return sum( (y - yfit)**2 )
 
@@ -17,10 +22,10 @@ def Jacobian( para, t, t0=-999 ):
     return [
         np.sin(2*np.pi*f1*(t-t0)),
         np.cos(2*np.pi*f1*(t-t0)),
-        2*np.pi*(t-t0)*(a11*np.sin(2*np.pi*f1*(t-t0)) + a12*np.cos(2*np.pi*f1*(t-t0))),
+        2*np.pi*(t-t0)*(a11*np.cos(2*np.pi*f1*(t-t0)) - a12*np.sin(2*np.pi*f1*(t-t0))),
         np.sin(2*np.pi*f2*(t-t0)),
         np.cos(2*np.pi*f2*(t-t0)),
-        2*np.pi*(t-t0)*(a21*np.sin(2*np.pi*f2*(t-t0)) + a22*np.cos(2*np.pi*f2*(t-t0)))
+        2*np.pi*(t-t0)*(a21*np.cos(2*np.pi*f2*(t-t0)) - a22*np.sin(2*np.pi*f2*(t-t0)))
     ]
 
 def Jacobian2( para, t, t0=-999 ):
