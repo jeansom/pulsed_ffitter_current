0033d6848622a1f5933104f2ae725f468e64b4c4
jeansom <jsomalwar@gmail.com>
2018-12-28 06:39:41 -0500

    Delete fitGammaRatio-checkpoint.py

--- .ipynb_checkpoints/fitGammaRatio-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-import numpy as np
-import scipy.optimize as optimize
-
-def fitGammaRatio( phi1_arr, phi2_arr, p0 ):
-    r_fit, r_cov = optimize.leastsq( lambda p: phi1_arr-p[0]*phi2_arr-p[1], [p0, 0.01] )
-    return r_fit[0]
